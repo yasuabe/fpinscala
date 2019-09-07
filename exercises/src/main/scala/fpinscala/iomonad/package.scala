147b69429ebb8a00ac580d29749176ead4a3966d
Yasuyuki Abe <yasu@yasuabe.net>
2019-09-08 02:01:19 +0900

    remove package object

--- exercises/src/main/scala/fpinscala/iomonad/package.scala
+++ exercises/src/main/scala/fpinscala/iomonad/package.scala
@@ -1,38 +1,36 @@
-package fpinscala
+package fpinscala.iomonad
 
 import language.higherKinds
 
-package object iomonad {
-  import java.util.concurrent.ExecutorService
-  import fpinscala.parallelism.Nonblocking._
-  import IO3.Free._
+import java.util.concurrent.ExecutorService
+import fpinscala.parallelism.Nonblocking._
+import IO3.Free.{Return => FreeReturn, _}
 
-  type IO[A] = IO3.IO[A]
-  def IO[A](a: => A): IO[A] = IO3.IO[A](a)
+type IO[A] = IO3.IO[A]
+def IO[A](a: => A): IO[A] = IO3.IO[A](a)
 
-  given ioMonad as Monad[({type f[a] = IO3.Free[Par, a]})#f] = IO3.freeMonad[Par]
+given ioMonad as Monad[({type f[a] = IO3.Free[Par, a]})#f] = IO3.freeMonad[Par]
 
-  def now[A](a: A): IO[A] = Return(a)
+def now[A](a: A): IO[A] = Return(a)
 
-  def fork[A](a: => IO[A]): IO[A] = par(Par.lazyUnit(())) flatMap (_ => a)
+def fork[A](a: => IO[A]): IO[A] = par(Par.lazyUnit(())) flatMap (_ => a)
 
-  def forkUnit[A](a: => A): IO[A] = fork(now(a))
+def forkUnit[A](a: => A): IO[A] = fork(now(a))
 
-  def delay[A](a: => A): IO[A] = now(()) flatMap (_ => now(a))
+def delay[A](a: => A): IO[A] = now(()) flatMap (_ => now(a))
 
-  def par[A](a: Par[A]): IO[A] = Suspend(a)
+def par[A](a: Par[A]): IO[A] = Suspend(a)
 
-  def async[A](cb: ((A => Unit) => Unit)): IO[A] =
-    fork(par(Par.async(cb)))
+def async[A](cb: ((A => Unit) => Unit)): IO[A] =
+  fork(par(Par.async(cb)))
 
-  type Free[F[_], A] = IO3.Free[F, A]
+type Free[F[_], A] = IO3.Free[F, A]
 
-  def Return[A](a: A): IO[A] = Return[Par,A](a)
+def Return[A](a: A): IO[A] = FreeReturn[Par,A](a)
 
-  // To run an `IO`, we need an executor service.
-  // The name we have chosen for this method, `unsafePerformIO`,
-  // reflects that is is unsafe, i.e. that it has side effects,
-  // and that it _performs_ the actual I/O.
-  def unsafePerformIO[A](io: IO[A]) given (E: ExecutorService): A =
-    Par.run { IO3.run(io) given IO3.parMonad }
-}
+// To run an `IO`, we need an executor service.
+// The name we have chosen for this method, `unsafePerformIO`,
+// reflects that is is unsafe, i.e. that it has side effects,
+// and that it _performs_ the actual I/O.
+def unsafePerformIO[A](io: IO[A]) given (E: ExecutorService): A =
+  Par.run { IO3.run(io) given IO3.parMonad }
